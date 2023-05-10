32c7ec7a58b46d717ca3b265083e7c3c01cdd81f
Oleksandr Lotko <lotkoolexandr@gmail.com>
2023-05-10 18:32:56 +0300

    add some changes to .eslintrc.cjs

--- src/modules/cart/Cart.tsx
+++ src/modules/cart/Cart.tsx
@@ -8,25 +8,22 @@ import s from './Cart.module.sass'
 
 const Cart = () => {
   const { isOpened, totalAmount, cartProducts, changeCartModalStatus } = useCartReducer()
-
   return (
-    <div>
-      {isOpened && (
-        <Modal
-          handleClose={changeCartModalStatus}
-          heading="Your Cart"
-          itemCount={cartProducts.length}>
-          <div className={s.productsListWrapper}>
-            <ModalProductList products={cartProducts} cartView />
-            <SummaryInfo totalAmount={totalAmount}>
-              <Link to="information">
-                <Button>Checkout</Button>
-              </Link>
-            </SummaryInfo>
-          </div>
-        </Modal>
-      )}
-    </div>
+    isOpened && (
+      <Modal
+        handleClose={changeCartModalStatus}
+        heading="Your Cart"
+        itemCount={cartProducts.length}>
+        <div className={s.productsListWrapper}>
+          <ModalProductList products={cartProducts} cartView />
+          <SummaryInfo totalAmount={totalAmount}>
+            <Link to="information">
+              <Button>Checkout</Button>
+            </Link>
+          </SummaryInfo>
+        </div>
+      </Modal>
+    )
   )
 }
 
