b7610bc16ed3c183cf140b619e974fc13c2abf00
Oleksandr Lotko <lotkoolexandr@gmail.com>
2024-01-20 19:16:27 +0200

    [V1] : [fix] - fix ts config & compinets imports

--- src/modules/cart/Cart.tsx
+++ src/modules/cart/Cart.tsx
@@ -4,26 +4,26 @@ import { ModalProductList, SummaryInfo } from 'components'
 import { Button, Modal } from 'UI'
 import { useCartReducer } from 'utils'
 
-import s from './Cart.module.sass'
-
 const Cart = () => {
   const { isOpened, totalAmount, cartProducts, changeCartModalStatus } = useCartReducer()
   return (
-    isOpened && (
-      <Modal
-        handleClose={changeCartModalStatus}
-        heading="Your Cart"
-        itemCount={cartProducts.length}>
-        <div className={s.productsListWrapper}>
-          <ModalProductList products={cartProducts} cartView />
-          <SummaryInfo totalAmount={totalAmount}>
-            <Link to="information">
-              <Button>Checkout</Button>
-            </Link>
-          </SummaryInfo>
-        </div>
-      </Modal>
-    )
+    <>
+      {isOpened && (
+        <Modal
+          handleClose={changeCartModalStatus}
+          heading="Your Cart"
+          itemCount={cartProducts.length}>
+          <div>
+            <ModalProductList products={cartProducts} cartView />
+            <SummaryInfo totalAmount={totalAmount}>
+              <Link to="information">
+                <Button>Checkout</Button>
+              </Link>
+            </SummaryInfo>
+          </div>
+        </Modal>
+      )}
+    </>
   )
 }
 
